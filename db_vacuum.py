58a74ec58e5accfd8b971888ecf0d6a3b4bde88c
odmin <maksobus@gmail.com>
2022-02-02 20:11:50 +0200

    Add vacuum db

--- db_vacuum.py
+++ db_vacuum.py
@@ -1,4 +1,5 @@
 import sys
+import shutil
 
 try:
     import sqlite3
@@ -6,8 +7,21 @@ except ImportError:
     print('\nsqlite3 module not installed. Run: pip install sqlite3')
     sys.exit()
 
+shutil.copy('testlabs.db', '/home/odmin/Документы/copy_base/testlabs.db')
+
 conn = sqlite3.connect('testlabs.db')
-sqlstr = '''VACUUM;'''
-conn.execute(sqlstr)
-conn.commit()
+
+sql_command = ['DELETE FROM addresses_esculab;',
+               'DELETE FROM addresses_newdiagnostics;',
+               'DELETE FROM addresses_synevo;',
+               'DELETE FROM cityes_esculab;',
+               'DELETE FROM cityes_synevo;',
+               'DELETE FROM tests_esculab;',
+               'DELETE FROM tests_newdiagnostics;',
+               'VACUUM;']
+
+for sqlq in sql_command:
+    print(sqlq)
+    conn.execute(sqlq)
+    conn.commit()
 conn.close()
